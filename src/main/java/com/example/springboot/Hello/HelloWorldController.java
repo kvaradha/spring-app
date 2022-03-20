f89deebd54dfa72cd595f30db26ba0f5d5edcdcf
karthi <karthikeyan.varadharajan@oracle.com>
2022-03-20 17:55:57 +0530

    First Dockerized Spring boot

---
+++ src/main/java/com/example/springboot/Hello/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.example.springboot.Hello;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class HelloWorldController {
+  
+  @RequestMapping("/")
+  String home() {
+    return "Hello World!";
+  }
+}
